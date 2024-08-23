58aa9b44a6c01f1bd51e18f6f53f259340ccca78
Vinicius Lobo Quirino <162513612+vqLobo@users.noreply.github.com>
2024-08-23 20:36:15 -0300

    Add files via upload

---
+++ atividade_1take2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	
+	double a, b, c;
+	
+	printf("Digite um numero (A): ");
+	scanf("%f", &a);
+	
+	printf("Digite um numero (B): ");
+	scanf("%f", &b);
+	
+	printf("Digite um numero (C): ");
+	scanf("%f", &c);
+	
+	
+	printf("%f", a);
+	
+	return(0);
+}
