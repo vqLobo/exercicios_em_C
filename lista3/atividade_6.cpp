2e87d24a84d9ed7852f88fda1e03258526f6f7e1
Vinicius Lobo Quirino <162513612+vqLobo@users.noreply.github.com>
2024-09-15 09:32:06 -0300

    Create atividade_6.cpp

---
+++ lista3/atividade_6.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//numeros pares de 0 a 100
+
+int main(){
+	
+    int num = 0;
+
+    while(num <= 100){
+        printf("%i \n", num);
+        num = num + 2;
+    }
+
+    printf("Achei facil :)");
+
+	return 0;
+}
