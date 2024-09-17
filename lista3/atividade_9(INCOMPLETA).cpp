9b928c88b55e422569721a49986c0fb6e4ff789c
Vinicius Lobo Quirino <162513612+vqLobo@users.noreply.github.com>
2024-09-17 19:23:46 -0300

    Update atividade_9.cpp

--- lista3/atividade_9(INCOMPLETA).cpp
+++ lista3/atividade_9(INCOMPLETA).cpp
@@ -12,25 +12,29 @@ int main(){
 
     printf("Digite outro numero: ");
     scanf("%i", &num2);
-
+    
     if(num1 < num2){
-        printf("%i eh menor que %i", num1, num2);
-    } else if (num2 < num1){
-        printf("%i eh menor que %i", num2, num1);
+            printf("%i eh menor que %i \n", num1, num2);
+        } else if (num2 < num1){
+            printf("%i eh menor que %i \n", num2, num1);
     }
-
-    while (num1 != num2 && num2 != num1){
-        //
-    }
-    if ((num1 < num2) || (num2 < num1)){
+    
+    while(num1 != num2){
+        
         printf("Digite outro numero: ");
         scanf("%i", &num1);
-
+        
         printf("Digite outro numero: ");
         scanf("%i", &num2);
-    } else if ((num1 == num2 & num2 == num1)){
-        printf("São iguais.");
+        
+        if(num1 < num2){
+            printf("%i eh menor que %i \n", num1, num2);
+        } else if (num2 < num1){
+            printf("%i eh menor que %i \n", num2, num1);
     }
-
+    }
+    
+    printf("São iguais");
+    
     return 0;
 }
