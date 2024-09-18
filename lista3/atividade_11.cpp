f21f8715c31bc13e730add821edcd2260b0f7674
Vinicius Lobo Quirino <162513612+vqLobo@users.noreply.github.com>
2024-09-17 21:51:08 -0300

    Update atividade_11.cpp

--- lista3/atividade_11.cpp
+++ lista3/atividade_11.cpp
@@ -6,14 +6,30 @@
 
 int main(){
 	
-    srand (time(NULL));
-    
-    int r = rand();
-    
-    r = ((r != 0) && (r <= 10));
-    
-    printf("%i", r);
-    
-    
+    int numero_aleatorio, chute;
+
+    const int limite = 10;
+
+    srand(time(NULL));
+
+    numero_aleatorio = rand() % (limite + 1);
+
+    printf("Tente acertar um numero escolhido aleatoriamente pela maquina: (de 1 a 10)"); 
+
+    do {
+        scanf("%i", &chute);
+
+        if(chute < 0 || chute > limite){
+            printf("escolha outro numero (entre 1 e 10)");
+        } else if (chute < numero_aleatorio){
+            printf("O numero que vc digitou eh menor que o numero aleatorio. ");
+        } else if (chute > numero_aleatorio){
+            printf("O numero que vc digitou eh maior que o numero aleatorio. ");
+        }
+
+    }   while (chute != numero_aleatorio);
+
+    printf("Parabens, conseguiu acertar o numero. Ele era %i", numero_aleatorio);
+
     return 0;
 }
